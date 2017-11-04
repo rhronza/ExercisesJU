5effa1a3bf22b6c08c55be2f043e98291182bba6
Roman Hronza <Roman Hronza@DESKTOP-36MHAVD>
2017-11-04 21:39:53 +0100

    c

--- src/main/java/mainpackage/App.java
+++ src/main/java/mainpackage/App.java
@@ -3,6 +3,7 @@ package mainpackage;
 /**
  * cvičení Java University ....
  * jedná se o samostatná cvičení, která provádím na základě toho co bylo probíráno v lekcích
+ * (poznámka)
  */
 public class App 
 {
