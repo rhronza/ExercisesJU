9ecc64aedb3fdb7f06f924db9ba83404d332af63
roman <roman@192.168.0.24>
2018-01-16 09:20:57 +0100

    c

--- src/main/java/mainpackage/VelikostBaleni.java
+++ src/main/java/mainpackage/VelikostBaleni.java
@@ -11,12 +11,12 @@ public class VelikostBaleni {
 		List<String> seznam = new ArrayList<String>();
 		seznam.add("Neutrogena Tělové mléko pro suchou pokožku 400ml");
 		seznam.add("Muumi Baby Jednorázové dětské bio pleny Maxi+ S5 10-16kg/22ks");
-		seznam.add("Beemy zavinovačka LIGHT STARS GREY warm");
+		seznam.add("Beemy zavinovačka LIGHT STARS GREY warm"); // 1ks
 		seznam.add("Beemy bryndáček/slintáček 2 ks Baby pink-purple");
 		seznam.add("Vinný Dům Alibernet 2015 výběr z hroznů 0,75l");
 		seznam.add("Beemy froté osuška 100x100cm PEPE"); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 		seznam.add("Coca - cola sklo 24 x 11ml");
-		seznam.add("Coca 3 x 4-cola sklo 24x200ml");
+		seznam.add("Coca - cola sklo 24x200ml");
 		seznam.add("Olma Revital jogurt angrešt s konopnými semínky 1,7% 145g");
 		seznam.add("Catsan Hygienické stelivo 10l");
 		seznam.add("Toro Houbička na nádobí kulatá „Smajlík” 79 x 30mm, 6ks");
@@ -47,7 +47,7 @@ public class VelikostBaleni {
 					velikostBaleniFloat = Float.parseFloat(velikostBaleniString);
 					System.out.println("9:" + s + ":" + velikostBaleniFloat + ", ");
 				} else {
-					pattern = Pattern.compile("(\\d+\\sx\\s\\d+)$");
+					pattern = Pattern.compile("(\\d+\\sx\\s\\d+)");
 					matcher = pattern.matcher(s);
 					if (matcher.find()) { /* obsahuje "číslo x číslo" */
 						velikostBaleniString = matcher.group(1);
